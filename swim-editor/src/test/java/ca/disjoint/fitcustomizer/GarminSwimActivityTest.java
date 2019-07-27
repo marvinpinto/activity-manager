6236c770396297a3bf31a569a9211bb95b3f29cc
Marvin Pinto <git@pinto.im>
2019-07-27 10:37:06 -0400

    Add another fit file test case

--- swim-editor/src/test/java/ca/disjoint/fitcustomizer/GarminSwimActivityTest.java
+++ swim-editor/src/test/java/ca/disjoint/fitcustomizer/GarminSwimActivityTest.java
@@ -1,4 +1,5 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
@@ -18,13 +19,32 @@ import com.garmin.fit.SubSport;
 public class GarminSwimActivityTest {
     private GarminSwimActivity activity;
 
-    @Before
-    public void setUp() throws FileNotFoundException, IOException {
-        URL url = this.getClass().getResource("/basic-swim.fit");
-        File basicSwimFitFile = new File(url.getFile());
+    private void setUpBasicSwimData() {
+        try {
+            URL url = this.getClass().getResource("/basic-swim.fit");
+            File basicSwimFitFile = new File(url.getFile());
 
-        activity = new GarminSwimActivity();
-        GarminActivityLoader gal = new GarminActivityLoader(basicSwimFitFile, activity);
+            activity = new GarminSwimActivity();
+            GarminActivityLoader gal = new GarminActivityLoader(basicSwimFitFile, activity);
+        } catch (FileNotFoundException ex) {
+            fail(ex.getMessage());
+        } catch (IOException ex) {
+            fail(ex.getMessage());
+        }
+    }
+
+    private void setUpMultipleLapSwimData() {
+        try {
+            URL url = this.getClass().getResource("/multiple-lap-swim.fit");
+            File basicSwimFitFile = new File(url.getFile());
+
+            activity = new GarminSwimActivity();
+            GarminActivityLoader gal = new GarminActivityLoader(basicSwimFitFile, activity);
+        } catch (FileNotFoundException ex) {
+            fail(ex.getMessage());
+        } catch (IOException ex) {
+            fail(ex.getMessage());
+        }
     }
 
     @After
@@ -33,7 +53,8 @@ public class GarminSwimActivityTest {
     }
 
     @Test
-    public void shouldParseSwimActivityCorrectly() {
+    public void shouldParseBasicSwimActivityCorrectly() {
+        setUpBasicSwimData();
         assertEquals(Sport.SWIMMING, activity.getSport());
         assertEquals(SubSport.LAP_SWIMMING, activity.getSubSport());
         assertEquals(22.86f, activity.getPoolLength(), 0.000);
@@ -43,5 +64,22 @@ public class GarminSwimActivityTest {
         assertEquals(294.146f, activity.getTotalElapsedTime(), 0.00);
         assertEquals(289.483f, activity.getTotalTimerTime(), 0.00);
         assertEquals("[Lap 1]   6 lengths (BREASTSTROKE) 00:04:40: BR,BR,BR,BR,BR,BR", activity.getLapSummary(0));
+    }
+
+    @Test
+    public void shouldParseMultipleLapSwimActivityCorrectly() {
+        setUpMultipleLapSwimData();
+        assertEquals(Sport.SWIMMING, activity.getSport());
+        assertEquals(SubSport.LAP_SWIMMING, activity.getSubSport());
+        assertEquals(17.0f, activity.getPoolLength(), 0.000);
+        assertEquals(74, activity.getNumActivePoolLengths());
+        assertEquals(14, activity.getNumTotalPoolLaps());
+        assertEquals(1258.0f, activity.getTotalDistance(), 0.00);
+        assertEquals(2773.603f, activity.getTotalElapsedTime(), 0.001);
+        assertEquals(2769.107f, activity.getTotalTimerTime(), 0.001);
+        assertEquals("[Lap 1]  10 lengths (BREASTSTROKE) 00:05:51: BR,BR,BR,BR,BR,BR,BR,BR,BR,BR",
+                activity.getLapSummary(0));
+        assertEquals("[Lap 7]   8 lengths (MIXED)        00:04:16: BR,BR,BR,BR,BR,BR,FR,BR", activity.getLapSummary(6));
+        assertEquals("", activity.getLapSummary(7));
     }
 }
